7c07b71355f6b64e7718171742f5184be0e1380f
goorst <zorind2004@mail.ru>
2024-09-12 18:37:50 +0300

    MTUSI_Project

---
+++ src/main/java/ru/mtuci/project/controller/DemoController.java
@@ -0,0 +1,17 @@
+package ru.mtuci.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam String str)
+    {
+        return "Hello World!";
+    }
+}
