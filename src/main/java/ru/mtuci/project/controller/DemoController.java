418aeae9dc873a8795536625f32c4302e3dcbbb7
goorst <zorind2004@mail.ru>
2024-09-29 11:36:26 +0300

    изменеия в Demo | DemoApplication | DemoController | pom.xml 29.09.2024

--- src/main/java/ru/mtuci/project/controller/DemoController.java
+++ src/main/java/ru/mtuci/project/controller/DemoController.java
@@ -1,17 +1,22 @@
 package ru.mtuci.project.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.mtuci.project.model.Demo;
 
 @RestController
 @RequestMapping("/")
 public class DemoController {
 
+    //("/") + ("/hello")
     @GetMapping("/hello")
-    public String sayHello(@RequestParam String str)
-    {
-        return "Hello World!";
+    public String getHello(@RequestParam String str) {
+        return str;
     }
+
+    @PostMapping
+    public Demo setValue(@RequestBody Demo demo) {
+        demo.setNumber(demo.getNumber() + 1);
+        return demo;
+    }
+
 }
